9e5842d6be4bdfa21797897fdfcb1e047a2cb201
Vishak Pillai <vishak.senkrish@gmail.com>
2024-02-19 12:39:11 -0800

    Fixed some bugs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Test.java
@@ -1,14 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
-
-
-
 
 @TeleOp
 
@@ -21,30 +17,29 @@ public class TeleOp_Test extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-
-        double tgtPower = -this.gamepad1.left_stick_y;
         backleft = hardwareMap.get(DcMotor.class, "backleft");
         frontleft = hardwareMap.get(DcMotor.class, "frontleft");
         backright = hardwareMap.get(DcMotor.class, "backright");
         frontright = hardwareMap.get(DcMotor.class, "frontright");
 
-        frontleft.setPower(tgtPower);
-        backleft.setPower(tgtPower);
-        frontright.setPower(tgtPower);
-        backright.setPower(tgtPower);
-
-        telemetry.addData("Status", "Initialized");
-        telemetry.addData("speed", frontleft.getPower());
-        telemetry.update();
-
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
         // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) {
-            telemetry.addData("Status", "Running");
-            telemetry.update();
+        double tgtPower;
 
+        tgtPower = -this.gamepad1.left_stick_y;
+
+        while (opModeIsActive()) {
+
+            frontleft.setPower(tgtPower);
+            backleft.setPower(tgtPower);
+            frontright.setPower(tgtPower);
+            backright.setPower(tgtPower);
+
+            telemetry.addData("Status", "walking");
+            telemetry.addData("speed", frontleft.getPower());
+            telemetry.update();
         }
     }
 }
