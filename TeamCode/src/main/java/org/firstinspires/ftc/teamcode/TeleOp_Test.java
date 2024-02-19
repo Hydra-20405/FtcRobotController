abf8794cf468dad97adea12045cd255658a3e71e
evanhuang <pumpkin.patch.evan@gmail.com>
2024-02-19 12:29:49 -0800

    gamepad test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Test.java
@@ -13,15 +13,30 @@ import com.qualcomm.robotcore.util.Range;
 @TeleOp
 
 public class TeleOp_Test extends LinearOpMode {
-
+    private DcMotor backleft;
+    private DcMotor frontleft;
+    private DcMotor backright;
+    private DcMotor frontright;
 
 
     @Override
     public void runOpMode() {
 
+        double tgtPower = -this.gamepad1.left_stick_y;
+        backleft = hardwareMap.get(DcMotor.class, "backleft");
+        frontleft = hardwareMap.get(DcMotor.class, "frontleft");
+        backright = hardwareMap.get(DcMotor.class, "backright");
+        frontright = hardwareMap.get(DcMotor.class, "frontright");
+
+        frontleft.setPower(tgtPower);
+        backleft.setPower(tgtPower);
+        frontright.setPower(tgtPower);
+        backright.setPower(tgtPower);
 
         telemetry.addData("Status", "Initialized");
+        telemetry.addData("speed", frontleft.getPower());
         telemetry.update();
+
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
 
