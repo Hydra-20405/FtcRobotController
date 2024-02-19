fa1ec9e99f883af64c0bcd75f65bb1c7de3e439a
evanhuang <pumpkin.patch.evan@gmail.com>
2024-02-19 12:01:45 -0800

    initial version of TeleOp_Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Test.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+
+
+@TeleOp
+
+public class TeleOp_Test extends LinearOpMode {
+
+
+
+    @Override
+    public void runOpMode() {
+
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+
+        }
+    }
+}
